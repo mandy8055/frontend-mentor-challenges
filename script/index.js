dc8d6b678c201bc90efe54ac39347b0b5f97ec3d
Manuj Sankrit <msankrit8055@gmail.com>
2023-05-13 12:55:22 +0530

    Revert "[CHAL-101][MANUJ] Fixed edge scenarios"
    This reverts commit bf5729f35f9befa76c4cdbbde13d00d65a0e08d1.

--- script/index.js
+++ script/index.js
@@ -6,16 +6,16 @@ const yearField = document.getElementById('year');
 // Submit Button
 const submitBtn = document.getElementById('submit-btn');
 
-// Result Spans
+// Result Span
 const yearsResult = document.getElementById('years-result');
 const monthsResult = document.getElementById('month-result');
 const daysResult = document.getElementById('days-result');
-const yearsText = document.getElementById('years-text');
-const monthsText = document.getElementById('months-text');
-const daysText = document.getElementById('days-text');
 
 function calculateDOB() {
-  resetFields();
+  // Reset age in result spans
+  yearsResult.textContent = '--';
+  monthsResult.textContent = '--';
+  daysResult.textContent = '--';
   if (!validateInputs(dayField.value, monthField.value, yearField.value))
     return;
   const formatter = `${yearField.value}-${monthField.value}-${dayField.value}`;
@@ -25,26 +25,13 @@ function calculateDOB() {
   const duration = moment.duration(diff);
 
   // Set age in result spans
-  displayResult(yearsResult, yearsText, duration.years(), 'year');
-  displayResult(monthsResult, monthsText, duration.months(), 'month');
-  displayResult(daysResult, daysText, duration.days(), 'day');
-}
-
-function displayResult(node1, node2, duration, trigger) {
-  node1.textContent = duration;
-  node2.textContent = duration === 1 ? trigger : `${trigger}s`;
-}
-
-function resetFields() {
-  yearsResult.textContent = '--';
-  yearsText.textContent = 'years';
-  monthsResult.textContent = '--';
-  monthsText.textContent = 'months';
-  daysResult.textContent = '--';
-  daysText.textContent = 'days';
+  yearsResult.textContent = duration.years();
+  monthsResult.textContent = duration.months();
+  daysResult.textContent = duration.days();
 }
 
 function validateInputs(day, month, year) {
+  console.log({ day, month, year });
   const maxDaysInMonth = moment()
     .set({ month: month - 1 })
     .daysInMonth();
@@ -75,22 +62,14 @@ function validateInputs(day, month, year) {
   }
 
   // check year
-  const dateToBeChecked = moment().add({
-    years: year - moment().year(),
-    months: month - (moment().month() + 1),
-    days: day - moment().date(),
-  });
   if (year === '') {
     displayError('This field is required', 'year');
     validYear = false;
   } else if (
     ((!validDay || !validMonth) && year > moment().year()) ||
-    dateToBeChecked.isAfter(moment(), 'day')
+    moment([year, month, day]).isAfter(moment(), 'day')
   ) {
     displayError('Must be in the past', 'year');
-    validYear = false;
-  } else if (year.toString().length < 4) {
-    displayError('Year should follow YYYY format', 'year');
     validYear = false;
   } else {
     removeError('year');
@@ -123,7 +102,6 @@ function displayError(errorMessage, trigger) {
 function removeError(trigger) {
   const input = document.getElementById(trigger);
   const inputContainer = input.closest('.inputs');
-  if (!input.classList.contains('error')) return;
 
   // remove error class from input and label
   input.classList.remove('error');
@@ -136,5 +114,4 @@ function removeError(trigger) {
   }
 }
 
-// Event handler
 submitBtn.addEventListener('click', calculateDOB);
